9852a4b978e7e8d51e36be22d1cc469f11914c43
Abdulla Al Roman <romannsu142@gmail.com>
2021-10-12 16:34:05 +0600

    Ecommerce project with Redux and api call

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+// Root reducer
+import { combineReducers } from 'redux';
+import productReducer from './productReducer'
+
+const rootReducer = combineReducers({
+	productReducer
+});
+
+export default rootReducer;
